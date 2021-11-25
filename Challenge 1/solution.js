3af54242052f899de2ff5c7d1c7d068246d671b0
Michael Le <72768694+mileuc@users.noreply.github.com>
2021-11-24 19:33:53 -0700

    Create solution.js

---
+++ Challenge 1/solution.js
@@ -0,0 +1,11 @@
+const parseMessage = (origin, message) => {
+  // Code here!
+  output = origin + ": " + message
+  // Want to see what's going on inside your function? 
+  // You can use console.log() to debug and print variables to the console below.
+  console.log(output)
+  // Remember to return a value!
+  return output
+}
+
+parseMessage("Mission Control", "Hello there!")
