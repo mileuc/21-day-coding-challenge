08bd34d392b99389f4d4832139e81c01f97ea466
Michael Le <72768694+mileuc@users.noreply.github.com>
2021-12-01 14:33:57 -0700

    Update solution.js

--- Challenge 10/solution.js
+++ Challenge 10/solution.js
@@ -7,3 +7,15 @@ const averageWindSpeed = (weatherEntries) => {
   avgWindSpeed = totalWindSpeed / (weatherEntries.length)
   return Math.round(avgWindSpeed)
 }
+
+// alternate solution
+const averageWindSpeed = (weatherEntries) => {
+  // Code here!
+  const reducer = (cumulativeWindSpeed, entry) => cumulativeWindSpeed + entry.windSpeed
+  totalWindSpeed = weatherEntries.reduce(reducer, 0) // 0 is used as the initial value for cumulative wind speed instead of the default first element of the array
+  // iteration therefore starts at the index 0 of the array instead of index 1
+  console.log(Math.round(totalWindSpeed/weatherEntries.length))
+  return Math.round(totalWindSpeed/weatherEntries.length)
+}
+// executes a user-supplied “reducer” callback function on each element of the array, in order, passing in the return value from the calculation on the preceding element. 
+// The final result of running the reducer across all elements of the array is a single value. 
