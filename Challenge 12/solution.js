f0219c56f8b973fb0d90339de574356d5ced8679
Michael Le <72768694+mileuc@users.noreply.github.com>
2021-12-03 14:35:57 -0700

    Create solution.js

---
+++ Challenge 12/solution.js
@@ -0,0 +1,16 @@
+// Add your parseMessage function here if you want!
+const parseMessage = (message) => {
+  new_string = `${message.origin}: ${message.message}`
+  return new_string
+}
+
+const parseTranscripts = (messages) => {
+  // Code here!
+  parsed_messages = []
+  messages.forEach(function(message){
+    parsed_message = parseMessage(message)
+    parsed_messages.push(parsed_message)
+  })
+  console.log(parsed_messages)
+  return parsed_messages
+}
